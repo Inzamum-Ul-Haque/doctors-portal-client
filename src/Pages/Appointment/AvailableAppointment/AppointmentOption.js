f84b7e719b2cebdfb14ccf39335d2fa3238b4f5d
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-11-14 20:25:20 +0600

    Sabing to Github

---
+++ src/Pages/Appointment/AvailableAppointment/AppointmentOption.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const AppointmentOption = ({ appointmentOption, setTreatment }) => {
+  const { name, slots } = appointmentOption;
+
+  return (
+    <div className="card shadow-lg">
+      <div className="card-body">
+        <h2 className="text-secondary font-bold text-2xl text-center">
+          {name}
+        </h2>
+        <p className="text-center">
+          {slots.length > 0 ? slots[0] : "Try another day"}
+        </p>
+        <p className="text-center">
+          {slots.length}
+          {slots.length > 1 ? "spaces" : "space"} available
+        </p>
+        <div className="card-actions justify-center">
+          <label
+            onClick={() => setTreatment(appointmentOption)}
+            htmlFor="booking-modal"
+            className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white"
+          >
+            Book Appointment
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AppointmentOption;
--- src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -1,12 +1,31 @@
 import { format } from "date-fns";
-import React from "react";
+import React, { useState } from "react";
+import BookingModal from "../BookingModal/BookingModal";
+import AppointmentOption from "./AppointmentOption";
 
 const AvailableAppointment = ({ selectedDate }) => {
+  const [appointmentOptions, setAppointmentOptions] = useState([]);
+  const [treatment, setTreatment] = useState(null);
+
+  fetch("appointments.json")
+    .then((res) => res.json())
+    .then((data) => setAppointmentOptions(data));
+
   return (
     <div className="mt-16">
       <p className="text-center text-secondary font-semibold">
         Available Appointments on {format(selectedDate, "PP")}
       </p>
+      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-5">
+        {appointmentOptions.map((option) => (
+          <AppointmentOption
+            key={option._id}
+            appointmentOption={option}
+            setTreatment={setTreatment}
+          ></AppointmentOption>
+        ))}
+      </div>
+      {treatment && <BookingModal treatment={treatment} />}
     </div>
   );
 };
