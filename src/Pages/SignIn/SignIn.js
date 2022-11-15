fb65510eccdffaa6a5adb29f8e2ac1b1206ea8fb
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-11-15 17:07:27 +0600

    Saving to Github

--- src/Pages/SignIn/SignIn.js
+++ src/Pages/SignIn/SignIn.js
@@ -1,40 +1,76 @@
-import React, { useState } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const SignIn = () => {
-  const { register, handleSubmit } = useForm();
-  const [data, setData] = useState("");
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const handleLogin = (data) => {
+    console.log(data);
+  };
 
   return (
     <div className="h-[800px] flex justify-center items-center">
-      <div>
-        <h1 className="text-xl">Sign In</h1>
-        <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+      <div className="w-96 p-8 border border-gray-300 shadow-xl">
+        <h1 className="text-2xl text-center">Sign In</h1>
+        <form onSubmit={handleSubmit(handleLogin)}>
           <div className="form-control w-full max-w-xs">
             <label className="label">
-              <label className="label-text">Email</label>
+              <span className="label-text">Email</span>
             </label>
             <input
               type="email"
-              {...register("email")}
+              {...register("email", { required: "Email address is required!" })}
               placeholder="Email"
               className="input input-bordered w-full max-w-xl"
             />
+            {errors.email && (
+              <p className="text-red-600">{errors.email?.message}</p>
+            )}
           </div>
           <div className="form-control w-full max-w-xs">
             <label className="label">
-              <label className="label-text">Password</label>
+              <span className="label-text">Password</span>
             </label>
             <input
               type="password"
-              {...register("password")}
+              {...register("password", {
+                required: "Password is required!",
+                minLength: {
+                  value: 6,
+                  message: "Password must be 6 characters or longer!",
+                },
+              })}
               placeholder="Password"
               className="input input-bordered w-full max-w-xl"
             />
+            {errors.password && (
+              <p className="text-red-600">{errors.password?.message}</p>
+            )}
+            <label className="label">
+              <span className="label-text">Forget Password?</span>
+            </label>
           </div>
-
-          <input type="submit" />
+          <input
+            className="btn btn-accent w-full mt-3"
+            value="Login"
+            type="submit"
+          />
         </form>
+        <p className="text-sm text-center mt-1">
+          New to Doctor's Portal?{" "}
+          <Link className="text-secondary" to="/sign-up">
+            Create a new account
+          </Link>
+        </p>
+        <div className="divider">OR</div>
+        <button className="btn btn-outline uppercase w-full">
+          Continue With Google
+        </button>
       </div>
     </div>
   );
