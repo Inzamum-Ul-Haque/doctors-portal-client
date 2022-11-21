ef1bff9074f8198bb8ba8cc12ddb68b5e0dc25a5
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-11-22 02:07:23 +0600

    Saving to Github

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -1,14 +1,21 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigation } from "react-router-dom";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "./CheckoutForm";
+import Loading from "../../Shared/Loading/Loading";
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
 
 const Payment = () => {
   const booking = useLoaderData();
+  const navigation = useNavigation();
   const { treatment, appointmentDate, price, slot } = booking;
+
+  if (navigation.state === "loading") {
+    return <Loading></Loading>;
+  }
+
   return (
     <div>
       <h3 className="text-3xl">Payment for {treatment}</h3>
@@ -18,7 +25,7 @@ const Payment = () => {
       </p>
       <div className="w-96 my-12">
         <Elements stripe={stripePromise}>
-          <CheckoutForm />
+          <CheckoutForm booking={booking} />
         </Elements>
       </div>
     </div>
